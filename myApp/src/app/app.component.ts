8eddc3bf9ac5fdac6245f918190edb2028c0c05e
hongzenan <1468425934@qq.com>
2017-03-20 20:12:45 +0800

    basic finish  ui structure of left side page

--- myApp/src/app/app.component.ts
+++ myApp/src/app/app.component.ts
@@ -9,6 +9,12 @@ import { HomePage } from '../pages/home/home';
 })
 export class MyApp {
   rootPage: any = HomePage
+  myInput: string = "";
+  toggleDate: boolean = false;
+  toggleItem1: boolean = false;
+  toggleItem2: boolean = false;
+  items1: string[] = ['item1 child1', 'item1 child2'];
+  items2: string[] = ['item2 child1', 'item2 child2'];
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
@@ -16,5 +22,26 @@ export class MyApp {
       Splashscreen.hide()
 
     });
+  }
+
+  onInput($event) {
+    console.log("onInput: ", $event);
+  }
+
+  onCancel($event) {
+    console.log("onCancel: ", $event);
+  }
+
+  toggleForItem1() {
+    this.toggleItem1 = !this.toggleItem1;
+  }
+
+  toggleForItem2() {
+    this.toggleItem2 = !this.toggleItem2;
+    console.log("item2", this.toggleItem2);
+  }
+
+  toggleForDate() {
+    this.toggleDate = !this.toggleDate;
   }
 }
