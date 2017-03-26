84abd616dae0e1454e550718c63f46bedfc18cda
hongzenan <1468425934@qq.com>
2017-03-26 20:58:54 +0800

    get tags and add tags with firebase

--- myApp/src/app/app.component.ts
+++ myApp/src/app/app.component.ts
@@ -1,9 +1,8 @@
-import { Component } from '@angular/core';
-import { Platform, AlertController, NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, AlertController} from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native'
 
 import { HomePage } from '../pages/home/home';
-import { AddTagPage } from '../pages/add-tag/add-tag';
 
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
 import * as firebase from 'firebase';
@@ -12,6 +11,7 @@ import * as firebase from 'firebase';
   templateUrl: 'app.html'
 })
 export class MyApp {
+  // @ViewChild('content') navCtrl: NavController;
   rootPage: any = HomePage
   user: any;
   email: string = "";
@@ -22,11 +22,12 @@ export class MyApp {
   toggleDate: boolean = false;
   toggleItem1: boolean = false;
   toggleItem2: boolean = false;
+  addTagItem: string;
   items1: string[] = [];
   items2: string[] = [];
 
   constructor(platform: Platform, public angfire: AngularFire,
-    public alertCtrl: AlertController, public navCtrl: NavController) {
+    public alertCtrl: AlertController) {
     platform.ready().then(() => {
       StatusBar.styleDefault()
       Splashscreen.hide()
@@ -42,7 +43,9 @@ export class MyApp {
     //get tags from firebase
     const database_tags = this.angfire.database.object('users/' + this.user_uid + '/tags');
     database_tags.subscribe(response => {
+      this.items1 = [];
       for (let i of response) {
+        console.log("i: ", i);
         this.items1.push(i);
       }
     });
@@ -73,7 +76,38 @@ export class MyApp {
   }
 
   addTag() {
-    this.navCtrl.push(AddTagPage);
+    console.log("tag: ");
+    let alertTag = this.alertCtrl.create({
+      title: '标签',
+      message: "请输入标签",
+      inputs: [
+        {
+          name: 'tag',
+          placeholder: 'Tag'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            this.addTagItem = data.tag;
+            this.items1.push(this.addTagItem);
+            console.log("data: ", data.tag);
+            const database_tags = this.angfire.database.object('users/' + this.user_uid + '/tags');
+            database_tags.update(this.items1);
+            console.log("dada: ", this.items1);
+          }
+        }
+      ]
+    });
+
+    alertTag.present();
   }
 
   toggleForItem2() {
